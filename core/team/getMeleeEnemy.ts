eaebdf7d1768a8d6f78db3dbebc841de1e23655e
Anastasia Aleksandrova <aleksandrovaanastasia2302@gmail.com>
2022-05-06 15:15:02 +0300

    fixed bug

--- core/team/getMeleeEnemy.ts
+++ core/team/getMeleeEnemy.ts
@@ -10,21 +10,6 @@ export const getMeleeEnemy = (allUnits: Unit[], currentUnit: string) => {
   const thirdLine = [6, 7, 8];
   const fourthLine = [9, 10, 11];
 
-  if (secondLine.includes(unitPosition)) {
-    if (includesAliveUnits(firstLine, allUnits)) {
-      return [];
-    }
-
-    const indexes =
-      unitPosition === 4
-        ? thirdLine
-        : thirdLine.filter((e) => thirdLine.indexOf(e) !== unitPosition - 3);
-
-    const targets = allUnits.filter((unit, i) => indexes.includes(i));
-
-    return targets;
-  }
-
   if (firstLine.includes(unitPosition)) {
     if (!includesAliveUnits(thirdLine, allUnits)) {
       const targets = allUnits.filter((unit, i) => fourthLine.includes(i));
@@ -37,8 +22,25 @@ export const getMeleeEnemy = (allUnits: Unit[], currentUnit: string) => {
         : thirdLine.filter((e) => thirdLine.indexOf(e) !== unitPosition);
 
     const targets = allUnits.filter((unit, i) => indexes.includes(i));
-
     return targets;
   }
+
+  if (secondLine.includes(unitPosition)) {
+    if (includesAliveUnits(firstLine, allUnits)) {
+      return [];
+    }
+
+    if (
+      !includesAliveUnits(firstLine, allUnits) &&
+      !includesAliveUnits(thirdLine, allUnits)
+    ) {
+      const targets = allUnits.filter((unit, i) => fourthLine.includes(i));
+      return targets;
+    }
+
+    const targets = allUnits.filter((unit, i) => thirdLine.includes(i));
+    return targets;
+  }
+
   return [];
 };
